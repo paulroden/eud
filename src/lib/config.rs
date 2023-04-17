6cd70b3e756a285b129cb65c80f11a7c2b66b39c
Paul Roden Henderson <git@paulroden.net>
2023-04-17 14:15:25 +0100

    add `alternative-editor` item to Config

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -4,13 +4,19 @@
 pub struct Config {
     pub default_socket: &'static str,
     pub tmp_dir: &'static str,
+    pub editor: &'static str,
 }
 
 impl Config {
-    pub fn new(default_socket: &'static str, tmp_dir: &'static str) -> Self {
+    pub fn new(
+        default_socket: &'static str,
+        tmp_dir: &'static str,
+        editor: &'static str
+    ) -> Self {
         Self {
             default_socket,
             tmp_dir,
+            editor,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use eud::config::Config;
 use eud::cli::cli;
 
-
 fn main() {
 
-    let config = Config::new("server", "/tmp/");
+    let config = Config::new(
+        "server",
+        "/tmp/",
+        "nano",
+    );
 
     match cli(&config) {
         Ok(_) => (),
         Err(e) => eprintln!("{}", e),
     };
-    
 }
