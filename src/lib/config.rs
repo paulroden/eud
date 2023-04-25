e60554bedf7405e91feeb51b636592d5fb9738e6
Paul Roden Henderson <git@paulroden.net>
2023-04-25 10:59:57 +0100

    clean up access to member variables

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -4,9 +4,9 @@ use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub struct Config {
-    pub default_socket: String,
-    pub server_socket_dir: PathBuf,   // c.f. `server-socket-dir' in emacs
-    pub editor: String,
+    default_socket: String,
+    server_socket_dir: PathBuf,   // c.f. `server-socket-dir' in emacs
+    editor: String,
 }
 
 impl Config {
@@ -36,8 +36,14 @@ impl Config {
             editor: "nano".to_string(),
         })
     }
+    pub fn default_socket_name(&self) -> &String {
+        &self.default_socket
+    }
     pub fn server_socket_dir(&self) -> &PathBuf {
         &self.server_socket_dir
+    }
+    pub fn alternative_editor(&self) -> &String {
+        &self.editor
     }
 }
 
@@ -54,26 +60,4 @@ fn expand_tilde_as_home<'p, P: AsRef<Path>>(path: &'p P) -> Cow<'p, Path> {
         false => path.into()
     }
 }
-
-
-// Socket Name
-// socket_name
-//   = as passed to cli (i.e. `new NAME`)
-//   | per environment $EMACS_SERVER_NAME (?)
-//   | config file default (?)
-//   | Emacs own default ("server"), cf. `server-name'  https://github.com/emacs-mirror/emacs/blob/4f3dae2b0d5fc43e5e2effa6d36544b6de2a43d8/lisp/server.el#L253
-
-// Socket File Directory
-// sockets_dir
-//   = as passed to cli (?)
-//   | per environment $EMACS_SOCKET_DIR (?)
-//   | config file default (?)
-//   | Emacs own default ($TMPDIR or /tmp)   i.e. `server-socket-dir' https://github.com/emacs-mirror/emacs/blob/4f3dae2b0d5fc43e5e2effa6d36544b6de2a43d8/lisp/server.el#L283
-// also consider making this a common variable read by Emacs' init and eud
-
-// Alternative Text Editor
-// alternative_editor
-// ...
-
-// others config variables?
 
